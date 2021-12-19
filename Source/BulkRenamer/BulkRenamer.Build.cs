9053b9267beab39660232c757e42207f9428d623
Yoann Potinet <yoann.potinet@frozenstorminteractive.com>
2021-12-19 16:41:31 -0500

    Clean BulkRenamer.Build.cs

--- Source/BulkRenamer/BulkRenamer.Build.cs
+++ Source/BulkRenamer/BulkRenamer.Build.cs
@@ -8,52 +8,26 @@ public class BulkRenamer : ModuleRules
 	{
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-
-
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-
-
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-
-
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Projects",
-				"InputCore",
-				"UnrealEd",
-				"LevelEditor",
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-                "EditorStyle",
-                "Json",
-                "JsonUtilities",
-            }
-			);
-
-
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
-	}
+        PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+		}
+		);
+        
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+            "CoreUObject",
+            "EditorStyle",
+            "Engine",
+            "InputCore",
+            "Json",
+            "JsonUtilities",
+            "LevelEditor",
+            "Projects",
+            "Slate",
+            "SlateCore",
+            "UnrealEd",
+        }
+		);
+    }
 }
