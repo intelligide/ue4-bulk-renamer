827e9f4e52de42487368fcd2bdfee06ed3660a70
Yoann Potinet <yoann.potinet@frozenstorminteractive.com>
2021-12-19 16:47:03 -0500

    Add the license to the top of the files

--- Source/BulkRenamer/Private/Rules/ReplaceRule.cpp
+++ Source/BulkRenamer/Private/Rules/ReplaceRule.cpp
@@ -1,11 +1,31 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019-2022 Yoann Potinet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #include "ReplaceRule.h"
 
 #define LOCTEXT_NAMESPACE "BulkRenamer"
 
 FString UReplaceRule::Apply(const FString& In) const
-{	
+{
 	return In.Replace(ToCStr(TextToBeReplaced), ToCStr(ReplaceWith), CaseSensitive ? ESearchCase::CaseSensitive : ESearchCase::IgnoreCase);
 }
 
